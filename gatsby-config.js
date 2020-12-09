e213356d9ecdd403e5fb4a7db0ace3a7535b2b60
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-12-09 15:22:01 -0500

    initial commit with logo added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Tiny Beet`,
+    description: `Using Lifestyle Medicine to nuture a better life for your children and family.`,
+    author: `Dr. Yolanda Rivera-Caudill`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
