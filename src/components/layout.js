1fa788297b608acaf053043a52201c0847d893a2
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-12-21 13:54:07 -0500

    rename root variable

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,9 +26,7 @@ const Layout = ({ children }) => {
   return (
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          margin: `0 auto`
         }}
       >
         <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
