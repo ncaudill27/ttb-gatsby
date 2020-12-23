1e50591c1889b951a3505085ceff3c0188ffc65a
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-12-23 12:44:46 -0500

    define basic Flex component

---
+++ src/components/flex.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './flex.module.css'
+
+const Flex = ({children, ...props}) => {
+  return (
+    <div className={styles.flex} {...props}>
+      {children}
+    </div>
+  )
+}
+
+export default Flex
