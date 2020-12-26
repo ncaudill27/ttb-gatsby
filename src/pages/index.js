fe32a449b1731972b749302396a6c50f4cacad13
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-12-26 10:09:02 -0500

    remove obsolete closeimage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -13,7 +12,11 @@ const IndexPage = () => (
     <Intro />
     <Section color='pri-dark'>
       <Dual section>
-        <h1 first>test</h1>
+        <div first>
+          <h1>For those who don’t know me, I’m Dr. Yolanda Rivera-Caudill</h1>
+          <p>Double board certified in American Academy of Pediatrics and Lifestyle Medicine. I’ve been practicing medicine for 32 years, having started in Miami then landing in Savannah—my home for the last 12 years.</p>
+          <p>I love making conversation with kids and seeing what comes out of their inquisitive minds. With the opening of my own practice, The Tiny Beet, I’ll be able to provide a more personalized approach to each patient and their families. Scroll on to read more on how we’ll make that possible / we’re structuring The Tiny Beet to make that happen.</p>
+        </div>
         <h2 second>test</h2>
       </Dual>
     </Section>
