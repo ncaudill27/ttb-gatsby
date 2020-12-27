6a3750bd9728e4da68d58ad0e15b1cfea464261e
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-12-27 12:21:33 -0500

    add Values component

---
+++ src/components/values.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Section from './section'
+import CirclesBackground from './backgrounds/circles'
+
+import BeetIcon from './images/beet'
+
+import styles from './values.module.css'
+
+const Values = () => (
+  <CirclesBackground color='sec-light'>
+    <Section >
+      <h1>Core Values</h1>
+      <div className={styles.flex}>
+        <div className={styles.item}>
+          <BeetIcon className={styles.icon} />
+          <div className={styles.copy}>
+            <h3>HEALTY EATING</h3>
+            <p>Enabling our patients to make better meal decisions</p>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <BeetIcon className={styles.icon} />
+          <div className={styles.copy}>
+            <h3>ACTIVE LIVING</h3>
+            <p>Guide patients on ways to get up and get moving</p>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <BeetIcon className={styles.icon} />
+          <div className={styles.copy}>
+            <h3>PROMOTING MINDFULNESS</h3>
+            <p>Empower our patients and their families to grow into their best selves</p>
+          </div>
+        </div>
+      </div>
+    </Section>
+  </CirclesBackground>
+)
+
+export default Values
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,18 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Mission from '../components/mission'
+import Quote from '../components/quote'
+import Values from '../components/values'
 
 const AboutPage = () => (
   <Layout>
     <SEO title='About' />
     <Mission />
+    <Quote
+      quote='Success is liking yourself, liking what you do, and liking how you do it.'
+      by='Maya Angelou'
+    />
+    <Values />
   </Layout>
 )
 
