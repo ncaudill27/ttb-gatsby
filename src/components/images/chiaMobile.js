2fbea7c6028794db5d942e34b8d9c4198215feb8
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-12-28 12:28:47 -0500

    change ChiaMobile component

---
+++ src/components/images/chiaMobile.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const ChiaMobile = (props) => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "chia-seeds.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 700) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  
+  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
+  return (
+    <div {...props}>
+      <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+    </div>
+  )
+}
+
+export default ChiaMobile
