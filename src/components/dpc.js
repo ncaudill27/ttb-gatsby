cd839e4190feffecfc708701f0a8d8e30586e134
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-01-04 14:21:02 -0500

    begin writing dpc section

---
+++ src/components/dpc.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useBreakpoint } from 'gatsby-plugin-breakpoints'
+
+import Section from './section'
+import Dual from './dual'
+import Button from './button'
+
+const DPCButton = () => <Button>Learn more about DPC</Button>
+
+const DPC = () => {
+  const breakpoints = useBreakpoint()
+  
+  return (
+    <Section color='sec-light'>
+      <Dual>
+        <div first>
+          <h1>
+            Why DPC?
+          </h1>
+          <p>
+            One of the things I’m most looking forward to in opening The Tiny Beet is spending more time with my patients.
+            A DPC model, similar to a concierge style, allows for this tailored experience and works as a monthly membership.
+            These are some of its advantages: 
+          </p>
+          { (breakpoints.md || breakpoints. l) && <DPCButton /> }
+        </div>
+        <div second>
+          
+          { breakpoints.sm && <DPCButton /> }
+        </div>
+      </Dual>
+    </Section>
+  )
+}
