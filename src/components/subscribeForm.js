6f267e697a1ad2bc3ca8023d16c65126f536bdc3
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-01-13 15:41:24 -0500

    add styling to text input

--- src/components/subscribeForm.js
+++ src/components/subscribeForm.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 
 import Form from './form'
 import Button from './button'
+import TextField from './textField'
 
 const SubscribeForm = (props) => {
 
   const [email, setEmail] = useState('')
+  const [firstName, setFirstName] = useState('')
+
   const handleChange = e => setEmail(e.target.value)
 
   const handleSubmit = e => {
@@ -23,8 +26,18 @@ const SubscribeForm = (props) => {
   return (
     <Form onSubmit={handleSubmit} {...props}>
       <h1>Subscribe</h1>
-      <label htmlFor='email'>Email</label>
-      <input id='email' value={email} onChange={handleChange} />
+      <TextField
+        id='email'
+        name={email}
+        placeholder='Please enter your email'
+        handleChange={handleChange}
+      />
+      <TextField
+        id='firstName'
+        name={firstName}
+        placeholder='Please enter your first name'
+        handleChange={handleChange}
+      />
       <Button>Subscribe to our newsletter</Button>
     </Form>
   )
---
+++ src/components/textField.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import styles from './textField.module.css'
+
+const TextField = ({id, name, placeholder, handleChange}) => {
+
+  const parseLabel = () => {
+    // space 'camelCase' into 'camel Case'
+    const titleizeRest = id.replace(/([A-Z])/g, ' $1')
+
+    // capitalize first letter and attach the rest
+    // resulting in 'camel Case' to 'Camel Case'
+    const titleized = titleizeRest.charAt(0).toUpperCase() + titleizeRest.slice(1)
+    
+    return titleized
+  }
+
+
+  return <div className={styles.container}>
+    <label htmlFor={id}>{parseLabel()}</label>
+    <input
+      id={id}
+      className={styles.input}
+      value={name}
+      onChange={handleChange}
+      placeholder={placeholder}
+    />
+  </div>
+
+}
+
+export default TextField
