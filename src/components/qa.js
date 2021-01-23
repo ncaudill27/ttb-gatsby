bfb5b499e32205d59256278b293e6ddcd21ef3b5
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-01-23 15:25:23 -0500

    animate FAQ open/close

--- src/components/qa.js
+++ src/components/qa.js
@@ -11,18 +11,16 @@ const QA = ({className, question, answer}) => {
 
   const rootStyles = isOpen ? classNames(styles.root, styles.open) : classNames(styles.root)
   const iconStyles = isOpen ? classNames(styles.icon, styles.open) : classNames(styles.icon)
-
+  const answerStyles = isOpen ? classNames(styles.answer, styles.open) : classNames(styles.answer)
   return (
     <li className={rootStyles} onClick={toggleOpen}>
       <div className={styles.question}>
         <p>{question}</p>
         <Chevron className={iconStyles} />
       </div>
-      {isOpen && (
-        <div className={styles.answer}>
+      {<div className={answerStyles}>
           {answer}
-        </div>
-      )}
+        </div>}
     </li>
   )
 }
