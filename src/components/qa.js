78b470c9e1c9fc0c0aa284beb93c52f9201efaec
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-01-26 12:05:17 -0500

    add accesibility to QA section

--- src/components/qa.js
+++ src/components/qa.js
@@ -8,19 +8,31 @@ const QA = ({question, answer}) => {
   
   const [isOpen, setIsOpen] = useState(false)
   const toggleOpen = () => setIsOpen( prev => !prev )
+  const handleEnterKey = e => {
+    if (e.keyCode === 13) toggleOpen()
+  }
 
   const rootStyles = isOpen ? classNames(styles.root, styles.open) : classNames(styles.root)
   const iconStyles = isOpen ? classNames(styles.icon, styles.open) : classNames(styles.icon)
   const answerStyles = isOpen ? classNames(styles.answer, styles.open) : classNames(styles.answer)
   return (
-    <li className={rootStyles} onClick={toggleOpen}>
+    <li className={rootStyles}>
+      <div
+        onClick={toggleOpen}
+        onKeyDown={handleEnterKey}
+        tabIndex={0}
+        role='button'
+        aria-haspopup='true'
+        aria-expanded={isOpen}
+      >
       <div className={styles.question}>
         <p>{question}</p>
-        <Chevron className={iconStyles} />
+        <Chevron className={iconStyles} aria-hidden='true' />
       </div>
       {<div className={answerStyles}>
         {answer}
       </div>}
+      </div>
     </li>
   )
 }
