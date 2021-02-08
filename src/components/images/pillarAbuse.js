a6ce9d97daa7125ad3e7f08acbeb6b3d11fdf21f
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-02-08 18:40:52 -0500

    add pillars icons

---
+++ src/components/images/pillarAbuse.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const AbusePillar = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "Pillars_Abuse.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  
+  if (!data?.logo?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
+  return (
+    <div {...props}>
+      <Img fluid={data.logo.childImageSharp.fluid} />
+    </div>
+  )
+}
+
+export default AbusePillar;
---
+++ src/components/images/pillarEating.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const EatingPillar = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "Pillars_Healthy-Eating.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  
+  if (!data?.logo?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
+  return (
+    <div {...props}>
+      <Img fluid={data.logo.childImageSharp.fluid} />
+    </div>
+  )
+}
+
+export default EatingPillar;
