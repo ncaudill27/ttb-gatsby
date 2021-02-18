fe81edf81c31924773490ae88d6ac05328bdb2c8
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-02-18 14:46:15 -0500

    update styling for testimonials component

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -13,7 +13,7 @@ const testimonials = [
   },
   {
     name: 'Quentin & Karen English',
-    testimonial: `Absolutely the best we could ever ask for in a doctor for my son. My family has been with Dr. Rivera-Caudill since the beginning (newborn) and we absolutely love her! She is family to us, not just our son’s doctor. She took care of him from birth in Miami when we used to live there and is now taking care of my son here in Georgia. 21 years together...and I wouldn’t have it any other way. Dr. Rivera-Caudill is an exceptional Pediatrician. She took care of our son with all his stomach issues, migraines, scoliosis, throughout the years. She spends quality time with all of her pts and follows up with calls at night, after hours, and when he had to see a specialist she called and made sure everything went well. I love the extra touch that she gives not just to us but to all of her patients. I don’t know what we would have done without Dr. Caudill. She explains everything on all of our visits. This is my last year with her, then our son has to go to a pcp doctor which is going to be hard for me. How many people can say they have had their same pediatrician with them their whole child life (childhood into adulthood) and that’s including moving from state to state and still made sure he stayed with Dr. Rivera-Caudill? I am blessed to be one!!`
+    testimonial: `Absolutely the best we could ever ask for in a doctor for my son. My family has been with Dr. Rivera-Caudill since the beginning (newborn) and we absolutely love her! She is family to us, not just our son’s doctor. She took care of him from birth in Miami when we used to live there and is now taking care of my son here in Georgia. 21 years together...and I wouldn’t have it any other way. Dr. Rivera-Caudill is an exceptional Pediatrician. She took care of our son with all his stomach issues, migraines, scoliosis, throughout the years. She spends quality time with all of her patients and follows up with calls at night, after hours, and when he had to see a specialist she called and made sure everything went well. I love the extra touch that she gives not just to us but to all of her patients. I don’t know what we would have done without Dr. Caudill. She explains everything on all of our visits. This is my last year with her, then our son has to go to a pcp doctor which is going to be hard for me. How many people can say they have had their same pediatrician with them their whole child life (childhood into adulthood) and that’s including moving from state to state and still made sure he stayed with Dr. Rivera-Caudill? I am blessed to be one!!`
   },
   {
     name: 'Maria Hernandez',
@@ -23,19 +23,13 @@ const testimonials = [
 
 const Testimonials = () => (
   <Section color='pri-med' background>
-    <div className={styles.root}>
-      <div className={styles.header}>
-        <Arch className={styles.arch} />
-        <h1 className={styles.title}>
-          Family <br />
-          Testimonials
-        </h1>
-      </div>
-      <div className={styles.grid}>
-        {testimonials.map( props => (
-          <Testimonial key={props.name} {...props} />
-        ))}
-      </div>
+    <h1>
+      Family Testimonials
+    </h1>
+    <div className={styles.grid}>
+      {testimonials.map( props => (
+        <Testimonial key={props.name} {...props} />
+      ))}
     </div>
   </Section>
 )
