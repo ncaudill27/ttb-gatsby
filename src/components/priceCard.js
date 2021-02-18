1cb70f6febae612589687872dcfe8c3bce4e8174
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-02-18 11:10:49 -0500

    update pricing section to reflect branding board

--- src/components/priceCard.js
+++ src/components/priceCard.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import styles from './priceCard.module.css'
 
-const PriceCard = ({price, pricing, details}) => (
+const PriceCard = ({price, ageRange, pricing, details}) => (
   <div className={styles.root}>
+    <h3 className={styles.pricing}>{pricing}</h3>
+    <p className={styles.age}>{ageRange} years</p>
     <h2 className={styles.price}><span className={styles.sign}>&#36;</span>{price}</h2>
-    <h4 className={styles.pricing}>{pricing}</h4>
-    <p>{details}</p>
+    <p className={styles.details}>{details}</p>
   </div>
 )
 
--- src/components/pricing.js
+++ src/components/pricing.js
@@ -9,56 +9,67 @@ import styles from './pricing.module.css'
 const prices = [
   {
     price: 120,
-    pricing: 'Sprouts \n0-2 y/o',
-    details: 'Blah blah blah'
+    pricing: 'Sprouts',
+    ageRange: '0-2',
+    details: 'Includes 3 home visits for the newborns: 1 to 2 days from hospital discharge (at 2 weeks of age and 1 month of age)'
   },
   {
-    price: 30,
-    pricing: 'Gonzalez Package',
-    details: 'Blah blah blah'
+    price: 80,
+    pricing: 'Seedlings',
+    ageRange: '2-5',
+    details: ''
   },
   {
-    price: 50,
-    pricing: 'Teenager Package',
-    details: 'Blah blah blah'
+    price: 70,
+    pricing: 'Lil\' Beets',
+    ageRange: '6-18',
+    details: 'Includes sports physicals'
   }
 ]
 
 const Pricing = () => (
   <Section color='sec-light' id='pricing'>
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>
-        Our <br/>
-        memberships <br/>
-        include:
-      </h1>
-      <div className={styles.lists}>
-        <ul className={styles.list}>
-          <li>Personal number to communicate with Dr. Caudill or scheduled Telemedicine.</li>
-          <li>Lifestyle Medicine education</li>
-          <li>Lactation support</li>
-          <li>Same day or next day appointments</li>
-          <li>30 minute blocked visits</li>
-          <li>Minimal to no wait time in waiting room</li>
-          <li>All well check up visits as per American Academy of Pediatrics (AAP)</li>
-          <li>Immunizations as per AAP</li>
-        </ul>
-        <ul className={styles.list}>
-          <li>Sick visits, same day or next, with appointment</li>
-          <li>Discounted price on medications</li>
-          <li>Most labs included</li>
-          <li>Incision/drainage and simple sutures</li>
-          <li>Chronic care management and coordinations</li>
-          <li>Developmental screenings per age group</li>
-          <li>Events and workshops at discounted rate</li>
-        </ul>
+      <div className={styles.wrapper} style={{marginBottom: '2rem'}}>
+        <h1 className={styles.title}>
+          Our <br/>
+          memberships <br/>
+          include:
+        </h1>
+        <div className={styles.lists}>
+          <ul className={styles.list}>
+            <li>Dr. Caudill’s direct number to communicate with</li>
+            <li>Telemedicine</li>
+            <li>Minimal to no wait time in waiting room</li>
+            <li>Same or next day sick visits, with appointment</li>
+            <li>30 minute blocked visits</li>
+            <li>All well check up visits as per American Academy of Pediatrics guidelines</li>
+          </ul>
+          <ul className={styles.list}>
+            <li>Discounted price on medications</li>
+            <li>Lactation support</li>
+            <li>Most labs included</li>
+            <li>Incision/drainage and simple sutures</li>
+            <li>Chronic care management and coordinations</li>
+            <li>Lifestyle Medicine education</li>
+            <li>Developmental screenings per age group</li>
+            <li>Events and workshops at discounted rate</li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
-      {prices.map(price => (
-        <PriceCard key={price.price} {...price} />
-      ))}
-    </div>
+      <div className={styles.wrapper}>
+        <div>
+          <h1 className={styles.tiers}>
+            Membership <br/>
+            Tiers:
+          </h1>
+          <p>Discounts available for 3 or more children</p>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+          {prices.map(price => (
+            <PriceCard key={price.price} {...price} />
+          ))}
+        </div>
+      </div>
   </Section>
 )
 
