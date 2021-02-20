d1c50044525c73f884d26c3b003336842fe141df
Nelson Caudill <nelson.a.caudill@gmail.com>
2021-02-20 03:01:33 -0500

    restyle footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,38 +11,41 @@ function Footer() {
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
         <FooterLogo className={styles.logo} />
-        <div className={styles.info}>
-          <div>
-            <h3>ADDRESS</h3>
-            <p>
-              51 Johnny Mercer Blvd. <br/>
-              Building A-2 Suite A<br/>
-              Savannah, GA 31400
-            </p>
-          </div>
-          <div>
-            <h3>SOCIAL</h3>
-            <div className={styles.icons}>
-              <FB className={styles.icon} />
-              <Insta className={styles.icon} />
+        <div className={styles.details}>
+          <div className={styles.stack}>
+            <div>
+              <h3>ADDRESS</h3>
+              <p>
+                51 Johnny Mercer Blvd. <br/>
+                Building A-2 Suite A<br/>
+                Savannah, GA 31400
+              </p>
+            </div>
+            <div>
+              <h3>PHONE</h3>
+              <p>(912) 777-6830</p>
+            </div>
+            <div>
+              <h3>EMAIL</h3>
+              <p>thedoctorbeet@gmail.com</p>
             </div>
           </div>
-          <div>
-            <h3>PHONE</h3>
-            <p>(912) 777-6830</p>
+          <div className={styles.stack}>
+            <div>
+              <h3>HOURS</h3>
+              <p>Monday - Friday, 9am - 4pm</p>
+            </div>
+            <div>
+              <h3>FIND US ON SOCIAL!</h3>
+              <div className={styles.icons}>
+                <Insta className={styles.icon} />
+                <FB className={styles.icon} />
+              </div>
+            </div>
+            <div className={styles.copy}>
+              <p>&copy; {new Date().getFullYear()} The Tiny Beet. All Rights Reserved.</p>
+            </div>
           </div>
-          <div>
-            <h3>EMAIL</h3>
-            <p>thedoctorbeet@gmail.com</p>
-          </div>
-          <div>
-            <h3>HOURS</h3>
-            <p>Monday - Friday, 9am - 4pm</p>
-          </div>
-        </div>
-        <div className={styles.copy}>
-          <p>&copy; {new Date().getFullYear()} The Tiny Beet. All Rights Reserved.</p>
-          <p>Created by Thunder & Lighting.</p>
         </div>
       </div>
     </footer>
